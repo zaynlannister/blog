e85b7599d22744e0542aff69f25396b5dea86965
zaynlannister <zaynlannister@gmail.com>
2022-11-09 10:05:23 +0500

    create Components & divide template

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,23 @@
 <template>
   <div class="app">
-    <h3>Создание поста</h3>
-    <div class="action">
-      <input class="input" type="text" placeholder="введите заголовок">
-      <input class="input action-inp" type="text" placeholder="введите содержимое">
-      <button class="btn action-btn">Создать</button>
-    </div>
+    <post-form/>
   </div>
 </template>
 
 <script>
-export default {
+import PostForm from "@/Components/PostForm.vue";
 
+export default {
+  components: {
+    PostForm,
+  }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
   .app {
     width: 1120px;
     margin: 0 auto;
-  }
-
-  .action {
-    display: flex;
-    flex-direction: column;
-    padding: 10px 0;
-
-    &-inp {
-      margin: 15px 0;
-    }
-
-    &-btn {
-      align-self: flex-end;
-    }
   }
 
   .input {
---
+++ src/Components/PostForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>Создание поста</h3>
+  <div class="action">
+    <input class="input" type="text" placeholder="введите заголовок">
+    <input class="input action-inp" type="text" placeholder="введите содержимое">
+    <button class="btn action-btn">Создать</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+  .action {
+    display: flex;
+    flex-direction: column;
+    padding: 10px 0;
+
+  &-inp {
+     margin: 15px 0;
+   }
+
+  &-btn {
+     align-self: flex-end;
+   }
+  }
+</style>
