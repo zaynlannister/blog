08d004e7d11759b1673d3c88405be0b6f08291dd
zaynlannister <zaynlannister@gmail.com>
2022-11-09 09:59:46 +0500

    rename class

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="app">
     <h3>Создание поста</h3>
-    <div class="app__action">
+    <div class="action">
       <input class="input" type="text" placeholder="введите заголовок">
-      <input class="input app__action-inp" type="text" placeholder="введите содержимое">
-      <button class="btn app__action-btn">Создать</button>
+      <input class="input action-inp" type="text" placeholder="введите содержимое">
+      <button class="btn action-btn">Создать</button>
     </div>
   </div>
 </template>
@@ -19,19 +19,19 @@ export default {
   .app {
     width: 1120px;
     margin: 0 auto;
+  }
 
-    &__action {
-      display: flex;
-      flex-direction: column;
-      padding: 10px 0;
+  .action {
+    display: flex;
+    flex-direction: column;
+    padding: 10px 0;
 
-      &-inp {
-        margin: 15px 0;
-      }
+    &-inp {
+      margin: 15px 0;
+    }
 
-      &-btn {
-        align-self: flex-end;
-      }
+    &-btn {
+      align-self: flex-end;
     }
   }
 
